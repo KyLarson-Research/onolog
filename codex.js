9511b75de113afb5ca94c0f9db296858b8904310
Kyle Larson <kylel@email.arizona.edu>
2020-11-13 20:23:25 -0700

    updated layout

--- codex.js
+++ codex.js
@@ -2,14 +2,26 @@
 // Author: Kyle C. Larson
 // Purpose: To help one prepare to answer short interviews
 // License: GNU General Public License 3 (GPL-3.0)
-const div = document.getElementById('bodyDiv');
+const qdiv = document.getElementById('questDiv');
 
-var x = document.createElement("FORM");
-x.setAttribute("id", "myForm");
-div.appendChild(x);
+var xq = document.createElement("FORM");
+xq.setAttribute("id", "myqForm");
+qdiv.appendChild(xq);
 
-var y = document.createElement("INPUT");
-y.setAttribute("type", "text");
-y.setAttribute("value", "enter a question");
-y.setAttribute("mAy", 3);
-document.getElementById("myForm").appendChild(y);
+var yq = document.createElement("INPUT");
+yq.setAttribute("type", "text");
+yq.setAttribute("value", "enter a question");
+yq.setAttribute("my", 3);
+document.getElementById("myqForm").appendChild(yq);
+
+
+const adiv = document.getElementById('ansDiv');
+var xa = document.createElement("FORM");
+xa.setAttribute("id", "myaForm");
+adiv.appendChild(xa);
+
+var ya = document.createElement("INPUT");
+ya.setAttribute("type", "text");
+ya.setAttribute("value", "enter a answer");
+ya.setAttribute("my", 4);
+document.getElementById("myaForm").appendChild(ya);
