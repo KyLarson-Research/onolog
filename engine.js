0ccf80c960f81c8f187e1f754eb316659a11cc59
Kyle Larson <kylel@email.arizona.edu>
2020-11-18 18:35:54 -0700

    updated engine.js to successfully serve from Public and Views

--- engine.js
+++ engine.js
@@ -1,16 +1,44 @@
+//Authored By Kyle Larson on August 28
+//Do not coppy, or redistribute the following source code is the intellectual property of KyLarson-Research
+//Pending Copyright(c) KyLarson-Research all rights reserved
 'use strict';
 var http = require('http');
-var fs = require('fs');
-var port = process.env.PORT || 8083;
+const fs = require('fs');
+var port = process.env.PORT || 1337;
+var path = require('path');
+var express = require('express');
 
-fs.readFile('./home.html', function (err, html) {
+var io = require('socket.io');
 
-    if (err) throw err;
+/*const server = http.createServer(function (req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    fs.readFile('./home.html', function (error, data) {
+        if (error) {
+            res.writeHead(404)
+            res.write('Error: File Not Found')//TODO create custom 404 page to write
+        } else {
+            res.write(data)
+        }
+        res.end()
+    });
+    //res.end('Hello World\n');
+});*/
 
-    http.createServer(function (req, res) {
-        res.writeHeader(200, { 'Content-Type': 'text/html' });
-        res.write(html);
-        res.end();
-    }).listen(port);
 
+var app = express();
+var server = http.createServer(app)
+server.listen(port, function (error) {
+    if (error) {
+        console.log('Something went wrong', error);
+    } else {
+        console.log('Server is listening on port ' + port);
+    }
+});
+
+app.engine('.html', require('ejs').__express);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'html');
+app.use(express.static(path.join(__dirname, 'public')));
+app.get('/', function(req, res){
+    res.render('home');
 });
